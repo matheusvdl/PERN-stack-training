4f0571b571ca47dd3a0d756371932541ac2e8997
Matheus Ribeiro <mribeiro@jazida.com>
2021-03-07 19:37:41 -0300

    Comitando pelo VScode

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -23,7 +23,7 @@ const InputTodo = ()=>{
 
     return(
         <Fragment>
-            <h1 className="text-center mt-5">Pern Todo List</h1>
+            <h1 className="text-center mt-5">Pern Todo List V1</h1>
             <form className="d-flex mt-5" onSubmit={onSubmitForm} >
                 <input type="text" 
                 className="form-control" 
